6df9865b8ed7047067460b82e82de686da9fe2fe
kumuditha-udayanga <kumudithaudaya@gmail.com>
2025-06-25 00:08:10 +0530

    Delete vivaPrep

--- src/VivaPrep/MyThread.java
+++ src/VivaPrep/MyThread.java
@@ -1,14 +1,23 @@
 package VivaPrep;
 
+import java.util.concurrent.Semaphore;
+
 class MyThread implements Runnable {
+    Semaphore sharedResource;
+    public MyThread(Semaphore sharedResource) {
+        this.sharedResource = sharedResource;
+    }
     @Override
     public void run () {
         for (int i = 1; i <= 10; i++) {
             System.out.println("My thread " + i + " " + Thread.currentThread().getName());
             try {
+                sharedResource.acquire();
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } finally {
+                sharedResource.release();
             }
         }
     }
